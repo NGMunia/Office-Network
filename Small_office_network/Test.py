f8333d150dfc808d63bf476b854bec0026e3f1f2
NGMunia <anthony.ngure@yahoo.com>
2023-07-09 15:00:48 +0300

    projects

--- Small_office_network/Test.py
+++ Small_office_network/Test.py
@@ -1,43 +1,46 @@
 
-
 from fastapi import FastAPI
 from netmiko import ConnectHandler
 from Device_list import RTR1, fW1, core
 import ntc_templates
 
 
-#Device-Health API:
 app = FastAPI()
-output =[]
 
+
+#Device-Health API:
+output =[]
 for devices in RTR1, fW1, core:
     conn = ConnectHandler(**devices)
     conn.enable()
-
     result = (conn.send_command('show version', use_textfsm=True))[0]
     output.append(result)
 
 @app.get('/Devices/health')
 def DeviceHealth():
-    return output
+    return{"RESPONSE": output}
+
+
 
 
 #Interfaces API:
 conn = ConnectHandler(**core)
 conn.enable()
-
 result = (conn.send_command('show ip interface brief', use_textfsm=True))
 
 @app.get('/Devices/Switches/Interfaces')
-def VlansAPI():
+def SwitchInterface():
     return {f'Host {core.get("ip")} interfaces':result}
+
+
 
 
 #VLANs API
 conn = ConnectHandler(**core)
 conn.enable()
-
 result = (conn.send_command("show vlan brief",use_textfsm=True))
+
 @app.get('/Devices/Switches/VLANs')
-def vlanAPI():
+def SwitchVlans():
     return result
+
